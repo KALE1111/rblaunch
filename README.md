2a9a556e44d22454d47c84a654ce3bb71b385d4b
SkylerPIlot <SkylerParthemore@gmail.com>
2023-06-06 11:14:46 -0700

    Initial Commit Still uses EthanInstaller(planned to change to our own repo)

---
+++ README.md
@@ -0,0 +1,5 @@
+GPL 3.0 sourced Plugins and Installer updated and maintained by RuneBot
+
+Plugins sourced from a variety of GPL sources such as Xkylee and Spoontob
+
+Installer and Launcher code Derived from EthanVann's implementation
---
+++ src/test/java/com/example/ExamplePluginTest.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import net.runelite.client.RuneLite;
+import net.runelite.client.externalplugins.ExternalPluginManager;
+
+public class ExamplePluginTest {
+    public static void main(String[] args) throws Exception {
+        RuneLite.main(args);
+    }
+}
